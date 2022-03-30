8a0584ad1db6358e4cd0f3dd3c59a446fcd58193
Luis Manzanero <manzanero.luis1995@gmail.com>
2022-03-30 23:21:36 +0800

    removed old code for markers

--- lib/ui/screens/home/widgets/map_view.dart
+++ lib/ui/screens/home/widgets/map_view.dart
@@ -4,14 +4,10 @@ import 'package:flutter_map/plugin_api.dart';
 // ignore: import_of_legacy_library_into_null_safe
 import "package:latlong/latlong.dart" as latLng;
 import 'package:mobile/view-models/PostViewModel.dart';
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:provider/provider.dart';
 
 class MapView extends StatelessWidget {
-  final List<latLng.LatLng> _markerPositions = [
-    latLng.LatLng(44.421, 10.404),
-    latLng.LatLng(45.683, 10.839),
-    latLng.LatLng(45.246, 5.783),
-  ];
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -34,27 +30,4 @@ class MapView extends StatelessWidget {
           ],
         ));
   }
-
-  List<Marker> get _markers => _markerPositions
-      .map(
-        (markerPosition) => Marker(
-          width: 60.0,
-          height: 60.0,
-          point: markerPosition,
-          builder: (ctx) => Container(
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.white, width: 2),
-                borderRadius: BorderRadius.all(Radius.circular(60))),
-            child: Padding(
-              padding: const EdgeInsets.all(1),
-              child: CircleAvatar(
-                backgroundImage: NetworkImage(
-                    "https://media0.giphy.com/media/b5xDLakZRxJ6M/200.gif"),
-                radius: 60.0,
-              ),
-            ),
-          ),
-        ),
-      )
-      .toList();
 }
