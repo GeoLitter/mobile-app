b3aa2537b3c23472c86cbc2677b52077f7db9070
Luis Manzanero <manzanero.luis1995@gmail.com>
2022-04-10 16:08:08 +0800

    added user details page

---
+++ lib/ui/screens/user/UserDetails.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UserDetails extends StatelessWidget {
+  const UserDetails({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("User Details"),
+    );
+  }
+}
