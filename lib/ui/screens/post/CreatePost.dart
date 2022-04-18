66d6f4ba5df555d130fd111e91991adf79cf2d3c
Luis Manzanero <manzanero.luis1995@gmail.com>
2022-04-18 23:23:17 +0800

    added empty skeleton create post screen

---
+++ lib/ui/screens/post/CreatePost.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CreatePost extends StatelessWidget {
+  const CreatePost({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Details',
+          style: TextStyle(color: Colors.black),
+        ),
+        leading: BackButton(
+          color: Colors.black54,
+        ),
+        automaticallyImplyLeading: true,
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: Container(
+        child: Text("Post"),
+      ),
+    );
+  }
+}
