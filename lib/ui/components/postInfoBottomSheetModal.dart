4c709a0e39ffd6c2f3731715a58b910d262461e9
Luis Manzanero <manzanero.luis1995@gmail.com>
2022-05-01 18:30:00 +0800

    added table with basic reserach quality fields

--- lib/ui/components/postInfoBottomSheetModal.dart
+++ lib/ui/components/postInfoBottomSheetModal.dart
@@ -16,7 +16,75 @@ void displayPostInfoBottomSheet(BuildContext context) {
           child: Padding(
             padding:
                 const EdgeInsets.only(left: 23, right: 23, bottom: 30, top: 20),
-            child: Text("Hello World"),
+            child: Container(
+              color: Colors.white,
+              child: DataTable(
+                columns: const <DataColumn>[
+                  DataColumn(
+                    label: Text(
+                      'Research Quality',
+                      style: TextStyle(fontStyle: FontStyle.italic),
+                    ),
+                  ),
+                  DataColumn(
+                    label: Text(
+                      'Yes',
+                      style: TextStyle(fontStyle: FontStyle.italic),
+                    ),
+                  ),
+                  DataColumn(
+                    label: Text(
+                      'No',
+                      style: TextStyle(fontStyle: FontStyle.italic),
+                    ),
+                  ),
+                ],
+                rows: const <DataRow>[
+                  DataRow(
+                    cells: <DataCell>[
+                      DataCell(Text('Date Specified')),
+                      DataCell(Icon(Icons.check)),
+                      DataCell(Text('')),
+                    ],
+                  ),
+                  DataRow(
+                    cells: <DataCell>[
+                      DataCell(Text('Location Specified')),
+                      DataCell(Icon(Icons.check)),
+                      DataCell(Text('')),
+                    ],
+                  ),
+                  DataRow(
+                    cells: <DataCell>[
+                      DataCell(Text('Location is Accurate')),
+                      DataCell(Icon(Icons.check)),
+                      DataCell(Text('')),
+                    ],
+                  ),
+                  DataRow(
+                    cells: <DataCell>[
+                      DataCell(Text('Community support')),
+                      DataCell(Text('')),
+                      DataCell(Icon(Icons.check)),
+                    ],
+                  ),
+                  DataRow(
+                    cells: <DataCell>[
+                      DataCell(Text('Evidence')),
+                      DataCell(Icon(Icons.check)),
+                      DataCell(Text('')),
+                    ],
+                  ),
+                  DataRow(
+                    cells: <DataCell>[
+                      DataCell(Text('Test')),
+                      DataCell(Text('')),
+                      DataCell(Icon(Icons.check)),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ),
         );
       });
